d622a7e9c7e1ccb5a580cc76f3096e1e3031faf3
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-19 10:02:11 +0700

    Login Page Design and DB Connect - Part 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const db_connect = require('./utils/db');
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const port = process.env.port;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+db_connect();
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ utils/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const db_connect = async () => {
+    try {
+        if (process.env.mode === 'production') {
+            await mongoose.connect(process.env.db_production_url)
+            console.log('Production database connect')
+        } else {
+            await mongoose.connect(process.env.db_local_url)
+            console.log('Local database connect')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+module.exports = db_connect 
