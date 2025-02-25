8c76790540bf45eaa2ff10b29c324fb6ed6bf2c6
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-25 10:31:21 +0700

    News Add Function - Part 4

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -1,7 +1,22 @@
+const {formidable} = require('formidable')
+const cloudinary = require('cloudinary').v2
+const newsModel = require('../models/newsModel')
+const {mongo: { ObjectId }} = require('mongoose')
+const moment = require('moment')
+
 class newsControllers {
 
     add_news = async (req,res) => {
-        console.log('add news')
+        // console.log('add news') 
+        const { id, name, category } = req.userInfo
+        const form = formidable({})
+
+        cloudinary.config({
+            cloud_name: process.env.cloud_name,
+            api_key: process.env.api_key,
+            api_secret: process.env.api_secret,
+            secure: true
+        })
     }
 }
 
