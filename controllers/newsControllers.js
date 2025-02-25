15c0cdbd46500399dd3a8636794bba3637e05657
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-25 10:38:40 +0700

    News Add Function - Part 5

--- controllers/newsControllers.js
+++ controllers/newsControllers.js
@@ -8,7 +8,7 @@ class newsControllers {
 
     add_news = async (req,res) => {
         // console.log('add news') 
-        const { id, name, category } = req.userInfo
+        const { id, name, category } = req.userInfoNew
         const form = formidable({})
 
         cloudinary.config({
@@ -16,7 +16,28 @@ class newsControllers {
             api_key: process.env.api_key,
             api_secret: process.env.api_secret,
             secure: true
-        })
+        });
+
+        try {
+            const [ fields, files ] = await form.parse(req)
+            const { url } = await cloudinary.uploader.upload(files.image[0].filepath, {folder: 'news_images'})
+            const {title, description} = fields
+
+            const news = await newsModel.create({
+                writerId: id,
+                writerName: name,
+                title: title[0].trim(), 
+                slug: title[0].trim().split(' ').join('-'),
+                category, 
+                description: description[0],
+                date: moment().format('LL'),
+                image: url
+            })
+            return res.status(201).json({message: 'News Added Successfully',news})
+            
+        } catch (error) {
+              return res.status(500).json({message: 'Internal server Error'})
+        } 
     }
 }
 
