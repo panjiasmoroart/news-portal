4350cd27a83be61c1d0fec82430a073ad25e0602
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-28 09:57:11 +0700

    App Create and Design Header - Part 1

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='bg-[#333333] text-[#cccccc]'>
+            <div className='px-5 lg:px-8 flex justify-between items-center py-2 border-b border-[#444444]'>
+                Header
+            </div>
+        </header>
+    );
+};
+
+export default Header;
