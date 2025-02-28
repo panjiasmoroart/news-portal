60ac61babe8c9b526a60127efb35907cfe33a3b6
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-28 13:21:53 +0700

    App Create and Design Header - Part 3

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,6 +3,10 @@ import moment from 'moment';
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
+import logo from '../assets/logo.png'
+import adver_image from '../assets/add.png'
+import bg_header from '../assets/header-bg.jpg'
+import Image from 'next/image';
 
 const Header = () => {
     return (
@@ -18,6 +22,30 @@ const Header = () => {
                     <a href="" className='w-8 h-8 flex justify-center items-center bg-[#ff5157] rounded-full hover:bg-slate-500 transition duration-200'><FaYoutube  /></a>
                 </div>
             </div>
+
+
+            <div style={{ backgroundImage: `url(${bg_header.src})` }} className='bg-cover bg-center text-center py-6'>
+                <div className='px-5 lg:px-8 flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0'>
+                    <div className='md:w-4/12 w-full flex flex-col items-center md:items-start space-y-3'>
+                        <Image 
+                        className='w-[200px] h-full'
+                        alt='logo'
+                        src={logo}
+                        priority
+                        />
+                        <h2 className='text-[#cccccc] text-md md:text-md font-semibold tracking-wide text-center md:text-left'>Media that rocks your world</h2>
+                    </div>
+
+                    <div className='md:w-8/12 w-full hidden md:flex justify-end'>
+                    <Image 
+                        className='max-w-full h-auto'
+                        alt='add'
+                        src={adver_image}
+                        priority
+                        />
+                    </div> 
+                </div> 
+            </div> 
         </header>
     );
 };
