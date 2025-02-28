52216a9aaf06b1d2e665bf45bc749d1c0d750800
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-28 16:55:18 +0700

    Latest News Design - Part 3

--- frontend/components/news/LatestNews.jsx
+++ frontend/components/news/LatestNews.jsx
@@ -11,15 +11,15 @@ const LatestNews = () => {
         superLargeDesktop: {
           // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
-          items: 5
+          items: 1
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 3
+          items: 1
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 2
+          items: 1
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
@@ -38,7 +38,7 @@ const LatestNews = () => {
                 transitionDuration={500}
             >
                 {
-                    [1,2,3,4].map((item, i) => <SimpleNewsCard />)
+                       [1,2,3,4].map((item, i) => <SimpleNewsCard item={item} key={i} type='latest' />)
                 }
 
             </Carousel>
--- frontend/components/news/item/SimpleNewsCard.jsx
+++ frontend/components/news/item/SimpleNewsCard.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const SimpleNewsCard = () => {
+const SimpleNewsCard = ({ item, type }) => {
     return (
         <div>
             <div className='group relative'>
                 <div className='overflow-hidden'>
-                    <div className='h-[250px] sm:h-[470px] w-full'>
-                        <Image className='' layout='fill' src={'https://res.cloudinary.com/dh1e1yjer/image/upload/v1740555216/news_images/bzklchn6ogs9rnpkldga.jpg'} alt='images' />
+                    <div className={`${type ? 'h-[270px] sm:h-[470px]' : 'h-[228px]'} w-full group-hover:scale-[1.1] transition-all duration-[1s]`}>
+                    <Image className='' layout='fill' src={'https://res.cloudinary.com/dbxtifnah/image/upload/v1727025863/news_images/hoz9yxkg1uhkufjhfdh3.png'} alt='images' />
                     </div>
                 </div>
+
+                <div className='w-full h-full block absolute left-0 top-0 invisible group-hover:visible bg-white cursor-pointer opacity-5 transition-all duration-300'>
+                </div>
+
+                <div className='left-5 absolute bottom-4 flex justify-start items-start flex-col text-white font-semibold gap-y-2'>
+                    <div className='px-[6px] py-[2px] rounded-md text-[13px] bg-[#c80000]'>
+                        Category
+                    </div> 
+                    <Link href={`/news/${item.slug}`} className='text-xl'>
+                        What puzzles reveal about the depths of our own
+                    </Link>
+                    <div className='flex gap-x-2 text-sm font-medium'>
+                        <span>28-02-2025</span>
+                        <span>By Panji Asmoro</span>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
