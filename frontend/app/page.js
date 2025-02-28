5e048ca4461717703bf0a46488f44e3b140a66cf
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-28 14:01:01 +0700

    Frontend Headline Design - react-fast-marquee

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HeadLines from "@/components/HeadLines";
 
 export default function Home() {
   return (
      <main>
+       <HeadLines />
       <h1>Main Page </h1>
      </main>
   );
