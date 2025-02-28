a383f430a221862f2f2ed5e905b073857979d6ca
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-02-28 13:42:58 +0700

    App Create and Design Header - Part 5

--- frontend/components/Header_Category.jsx
+++ frontend/components/Header_Category.jsx
@@ -3,11 +3,13 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
 import { IoMdList } from "react-icons/io"; 
+import { IoMdCloseCircle } from "react-icons/io";
+import { FaSearch } from "react-icons/fa"; 
 
 const Header_Category = () => {
 
     const path = usePathname();
-    
+
     const data = [
         {
             id: 1,
@@ -35,7 +37,8 @@ const Header_Category = () => {
         },
     ]
 
-    const [cate_show, set_cate_show] = useState(false)
+    const [cate_show, set_cate_show] = useState(false);
+    const [show, setShow] = useState(false);
 
     return (
         <div className='w-full'>
@@ -51,8 +54,29 @@ const Header_Category = () => {
                             data.map((c,i) => <Link key={i} className={`px-6 font-medium py-[13px] ${path === c.name ? 'bg-[#00000026]' : ''} `} href={'/'} > { c.name} </Link>)
                         }
                     </div>
-                </div>
-            </div>
+
+                    <div className='h-full w-[50px]'>
+                        <div onClick={() => setShow(!show)} className={`text-xl ${show ? 'bg-[#00000026]' : ''} font-bold h-full w-full cursor-pointer justify-center flex items-center hover:bg-[#00000026]`} >
+                            {
+                                show ? <IoMdCloseCircle /> : <FaSearch />
+                            } 
+                        </div>
+
+                        <div className={`absolute lg:block transition-all text-slate-700 z-20 shadow-lg lg:right-10 top-[50px] w-full lg:w-[300px] right-0 ${show ? 'visible' : 'invisible'} `}>
+                            <div className='p-3 bg-white'>
+                                <form className='flex'>
+                                    <div className='w-[calc(100%-45px)] h-[40px]'>
+                                        <input type="text" placeholder='Seach' className='h-full w-full p-2 border border-slate-300 outline-none bg-slate-100' />  
+                                    </div>
+                                <button className='w-[45px] hover:bg-blue-700 cursor-pointer h-[40px] flex justify-center outline-none items-center bg-blue-600 text-white text-xl'>
+                                <FaSearch />
+                                </button>
+                                </form>
+                            </div>
+                        </div>    
+                    </div>
+                </div>  {/* end div px-8  */}
+            </div>  {/* end div bg-  */}
             
         </div>
     );
