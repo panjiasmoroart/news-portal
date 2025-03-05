f42ca0ff9c5a1b3b83a97da8e978631eb862d946
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-03-05 19:57:59 +0700

    Recent News Show - Part 2

--- frontend/components/RecentNewsFooter.jsx
+++ frontend/components/RecentNewsFooter.jsx
@@ -1,8 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { base_api_url } from '@/config/config';
 
-const RecentNewsFooter = () => {
+const RecentNewsFooter = async () => {
+    const res = await fetch(`${base_api_url}/api/recent/news`,{
+        next: {
+            revalidate: 1
+        }
+    })
+   
+    const {news} = await res.json() 
+
     return (
         <div className='w-full flex flex-col gap-y-[14px]'>
             <div className={`text-xl font-bold text-white relative before:absolute before:w-[4px] before:bg-[#5271ff] before:h-full before:-left-0 pl-3`}>
@@ -11,7 +20,7 @@ const RecentNewsFooter = () => {
 
             <div className='grid grid-cols-1 gap-y-2 pt-1'>
                 {
-                    [1,2,3].map((r, i) => {
+                   news && news.length > 0 && news.map((r, i) => {
                         if (i < 4) {
                             return <Link key={i} href={`/`} className='flex w-full'>
                                 <div className='group relative overflow-hidden w-[80px] h-[65px]'>
@@ -19,7 +28,7 @@ const RecentNewsFooter = () => {
                                         <Image
                                         className=''
                                         layout='fill'
-                                        src={'https://res.cloudinary.com/dh1e1yjer/image/upload/v1740468062/news_images/cdkbp3hjqcs5vaqqt0gs.jpg'}
+                                        src={r.image}
                                         alt='images'                
                                         />  
                                         <div className='w-full h-full block absolute left-0 top-0 invisible group-hover:visible bg-white cursor-pointer opacity-5 transition-all duration-300' href={"#"}> 
@@ -29,10 +38,10 @@ const RecentNewsFooter = () => {
 
                                 <div className='w-[calc(100%-90px)] pl-2'>
                                     <div className='flex flex-col gap-y-1'>
-                                        <h2 className='text-sm font-semibold text-white hover:text-[#c80000]'>What puzzles reveal about </h2>
+                                        <h2 className='text-sm font-semibold text-white hover:text-[#c80000]'>{r.title} </h2>
                                         <div className='flex gap-x-2 text-xs font-normal text-white'>
-                                    <span>20-09-2024</span>
-                                    <span>By Panji Asmoro</span>
+                                            <span>{r.date}</span>
+                                            <span>By {r.writerName}</span>
                                         </div> 
                                     </div> 
                                 </div>
